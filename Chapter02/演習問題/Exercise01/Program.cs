826fa1b55c3c397c9413d9ff925f2d60be0a1385
聖哉 大川 <oibc37165@ojs.ac.jp>
2026-05-13 10:19:06 +0900

    問題2.1完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -2,22 +2,46 @@
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
-            // 2.1.3
-            var songs = new Song[] {
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
-            };
-            PrintSongs(songs);
-        }
-        //2.1.4
-        private static void PrintSongs(Song[] songs) {
-            foreach (var song in songs) {
-                Console.WriteLine($"{song.Title},{song.ArtistName},{song.Length / 60}:" +
-                    $"{song.Length % 60}");
+
+            var songs = new List<Song>();
+            while(true) {
+                
+                Console.Write("曲名：");
+                string? title = Console.ReadLine();
+                if(title.Equals("end")) {
+                    break;
+                }
+                Console.Write("アーティスト名：");
+                string? artistname = Console.ReadLine();
+
+                Console.Write("演奏時間(秒)：");
+                string? length = Console.ReadLine();
+                int count = int.Parse(length);
+
+                Song song = new Song(title, artistname, count);
+
+                songs.Add(song);
+                PrintSongs(songs);
+
             }
         }
+        //2.1.4
+        private static void PrintSongs(IEnumerable<Song> songs) {
+            foreach (var song in songs) {
+                Console.WriteLine($"{song.Title},{song.ArtistName},{song.Length / 60}:{(song.Length % 60):00}");
+            }
+        }
+
+        // 2.1.3
+        //var songs = new Song[] {
+        //    new Song(Title,ArtistName,count)
+        //new Song("Let it be", "The Beatles", 243),
+        //new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+        //new Song("Close To You", "Carpenters", 276),
+        //new Song("Honesty", "Billy Joel", 231),
+        //new Song("I Will Always Love You", "Whitney Houston", 273),
+        //};
+        //PrintSongs(songs);
+
     }
 }
