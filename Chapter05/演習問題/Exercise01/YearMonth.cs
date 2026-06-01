35c40498133a5590938d7438d753d779c73839b0
聖哉 大川 <oibc37165@ojs.ac.jp>
2026-06-01 11:04:29 +0900

    5.1.2完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -5,16 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise01 {
-    //5.1.1
+    //5.1.1 (P114)
     public class YearMonth {
 
         public int Year { get; init; }
 
         public int Month { get; init; }
 
+        //5.1.2 (P116)
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
+
         public YearMonth(int year, int month) {
             Year = year;
             Month = month;
         }
+
+        //public YearMonth AddOneMonth() {
+        //    if ()
+            
+        //    return ;
+        //}
     }
+
 }
