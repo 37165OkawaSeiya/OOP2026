1451de2c78fc74624fcea6c4b7af9891d5c01eff
聖哉 大川 <oibc37165@ojs.ac.jp>
2026-06-10 10:40:35 +0900

    6.2完成

---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str = Console.ReadLine();
+            if (int.TryParse(str, out var num)) {
+                Console.WriteLine($"{num:#,0}");
+            } else {
+                Console.WriteLine("int型ではありません");
+            }
+        }
+    }
+}
