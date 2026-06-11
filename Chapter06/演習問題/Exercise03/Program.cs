868582921e353c39327379ae80213f3cfd3879f8
聖哉 大川 <oibc37165@ojs.ac.jp>
2026-06-11 10:50:25 +0900

    6.3.6

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -42,17 +42,35 @@ namespace Exercise03 {
 
         private static void Exercise4(string text) {
             var cnt = text.Split(' ').Length;
-            Console.WriteLine(cnt);
+            Console.WriteLine($"単語数:{cnt}");
         }
 
         private static void Exercise5(string text) {
-            var cnt = text.Split(' ').Where(s => s.Length == 4);
+            var cnt = text.Split(' ').Where(s => s.Length <= 4);
             foreach (var s in cnt) {
                 Console.WriteLine(s);
             }
         }
 
+        //アルファベットの数をカウントして表示する
         private static void Exercise6(string text) {
+            var str = text.ToLower().Replace(" ", "");
+
+            //辞書（ディクショナリを使った集計
+            //var alphDicCount = Enumerable.Range('a', 26).
+            //    ToDictionary(num => ((char)num).ToString(), num => 0);
+
+            var dict = new SortedDictionary<char, int>();
+            foreach (var c in str) {
+                if (dict.ContainsKey(c))
+                    dict[c]++;//登録されている場合
+                else
+                    dict[c] = 1;//未登録の場合
+            }
+            foreach (var word in dict) {
+                Console.WriteLine(word.Key + ":" + word.Value);
+            }
+
 
         }
     }
