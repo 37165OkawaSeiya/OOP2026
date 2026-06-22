eebe6e52abea7526ce4fcc2de898c095f3a675c9
聖哉 大川 <oibc37165@ojs.ac.jp>
2026-06-22 11:33:34 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,7 +1,10 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
-
-
+        //学生の名前
+        public required string Name { get; init; }
+        //科目名
+        public required string Subject { get; init; }
+        //点数
+        public required int Score { get; init; }
     }
 }
