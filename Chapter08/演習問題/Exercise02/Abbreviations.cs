566962705611131c503cfd23c1fd2e85412603bc
聖哉 大川 <oibc37165@ojs.ac.jp>
2026-06-30 15:50:45 +0900

    8.2.1     8.2.2完成

--- Chapter08/演習問題/Exercise02/Abbreviations.cs
+++ Chapter08/演習問題/Exercise02/Abbreviations.cs
@@ -1,18 +1,15 @@
-namespace Section03;
 
-// 略語と対応する日本語を管理するクラス
+using System.Threading.Channels;
+
 public class Abbreviations {
-    private readonly Dictionary<string, string> _dict = new();
-    
-    // コンストラクター
+    private readonly Dictionary<string, string> _dict = [];
+
+    // コンストラクタ
     public Abbreviations() {
         var lines = File.ReadAllLines("./Abbreviations.txt");
-        _dict = lines
-            .Select(line => line.Split('='))
-            .ToDictionary(x => x[0], x => x[1]);
+        _dict = lines.Select(line => line.Split('='))
+                     .ToDictionary(x => x[0], x => x[1]);
     }
-    
-    
 
     // 要素を追加
     public void Add(string abbr, string japanese) =>
@@ -35,5 +32,19 @@ public class Abbreviations {
         }
     }
 
-    
+    // 8.2.1
+    //Countプロパティを追加
+    public int Count => _dict.Count;
+
+
+
+    // 8.2.2（最終的に式形式で記述すること）
+    public bool Remove(string abb) => _dict.Remove(abb);
+
+    // Dictionary<TKey, TValue>は、
+    // IEnumerable<KeyValuePair<TKey, TValue>>を実装しているので
+    // _dictを返せば良い
+    public IEnumerable<KeyValuePair<string, string>> GetAll() {
+        return _dict;
+    }
 }
