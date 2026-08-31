06c220f4e39e43eea17e6463b877d73052a4ea75
聖哉 大川 <oibc37165@ojs.ac.jp>
2026-08-31 14:12:42 +0900

    Databaseクラス完成

---
+++ FormApps/SQLiteProductSample/Database.cs
@@ -0,0 +1,48 @@
+using Microsoft.Data.Sqlite;
+
+namespace SQLiteProductSample;
+
+// SQLiteデータベースへの接続と初期化を担当するクラス
+public static class Database 
+{
+    //DBファイルへの保存場所
+    private static readonly string DatabasePath =
+        Path.Combine(AppContext.BaseDirectory, "products.db");
+
+    //SQLiteへ接続するための接続文字列
+    private static readonly string ConnectionString =
+        $"Data Source={DatabasePath}";
+
+    // DBファイルの保存場所を外部から確認するための読み取り専用プロパティ
+    public static string FilePath => DatabasePath;
+
+    public static SqliteConnection GetConnection() {
+        return new SqliteConnection(ConnectionString);
+    }
+
+    //DBの初期化処理
+    public static void Initilize() {
+        //接続オブジェクトを生成する。
+        using var connection = GetConnection();
+
+        //DBを開く
+        connection.Open();
+
+        //SQLを実行するためのコマンドオブジェクトを作る
+        using var command = connection.CreateCommand();
+
+        //IF NOT EXISTS により、既にテーブルがあってもエラーにならない
+        command.CommandText =
+            """
+            CREATE TABLE NOT EXISTS Products(
+                Id      INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name    TEXT NOT NULL,
+                Price   INTEGER NOT NULL CHECK (PRICE >= 0)
+            );
+            """;
+
+        //結果業を返さないSQLを実行する
+        command.ExecuteNonQuery();
+    }
+
+}
